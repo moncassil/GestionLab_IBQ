f2a9286878b6d0192b249e00745463542160cb3b
Gabriel Orive <35276201+Gaboshok@users.noreply.github.com>
2019-05-02 22:03:48 -0500

    Cree el codigo de validar.php

---
+++ validar.php
@@ -0,0 +1,23 @@
+<?php
+
+$usuario=$_POST['usuario'];
+$password=$_POST['password'];
+
+//conectar a la bd 
+$conexion=mysqli_connect("localhost","root","","ibq");
+$consulta="SELECT * FROM usuarios WHERE usuario='$usuario' and contrasena='$password'";
+$resultado=mysqli_query($conexion, $consulta);
+
+
+$filas=mysqli_num_rows($resultado);
+if($filas>0)
+{
+	header("location:Adm.html");
+}
+else
+{
+echo "error en la autenticidad";
+}
+
+mysqli_free_result($resultado);
+mysqli_close($conexion);
