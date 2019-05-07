2724427dffe3a65ea0edf173d68652f45ff9eab6
Gabriel Orive <35276201+Gaboshok@users.noreply.github.com>
2019-05-06 23:55:13 -0500

    Cambio de sentencias en registrar_persona.php
    Se cambio al video que te mande

--- registrar_persona.php
+++ registrar_persona.php
@@ -1,6 +1,6 @@
 <?php
-
 $conexion=mysqli_connect("localhost","root","","ibq");
+
 $id_persona=$_POST['id_persona'];
 $Nombre=$_POST['Nombre'];
 $Apellido_Paterno=$_POST['Apellido_Paterno'];
@@ -10,22 +10,12 @@ $Telefono=$_POST['Telefono'];
 $Comentarios=$_POST['Comentarios'];
 
 
+$sql ="INSERT INTO persona(id_persona,nombre_persona,ape_pat,ape_mat,tel_persona,cargo_persona,coment_persona)
+VALUES('$id_persona','$Nombre','$Apellido_Paterno','$Apellido_Materno','$Cargo','$Telefono','$Comentarios')";
 
-
-$sqlInsertTipo="INSERT INTO persona(id_persona,nombre_persona,ape_pat,ape_mat,tel_persona,cargo_persona,coment_persona) VALUES($id_persona,$Nombre,$Apellido_Paterno,$Apellido_Materno,$Cargo,$Telefono,$Comentarios)";
-
-
-
-if($rs=$conexion->query($sqlInserTipo))
-{
-echo 'texto de prueba';
-echo '<a href="usuario.html"> aceptar </a>';
-}
-else
-{
-echo 'error al encontrar la base de datos';
-}
+mysqli_query($conn, $sql);
+header("Location: ./usuario.html?registro=hecho!");
 
 mysqli_close($conexion);
-?>
+
 
