9141b75fd3811bcd091f7fabfea1f896804eeedc
Gabriel Orive <35276201+Gaboshok@users.noreply.github.com>
2019-05-06 23:02:53 -0500

    Update registrar_persona.php

--- registrar_persona.php
+++ registrar_persona.php
@@ -1,6 +1,6 @@
 <?php
 
-
+$conexion=mysqli_connect("localhost","root","","ibq");
 $id_persona=$_GET['id_persona'];
 $Nombre=$_GET['Nombre'];
 $Apellido_Paterno=$_GET['Apellido_Paterno'];
@@ -10,7 +10,7 @@ $Telefono=$_GET['Telefono'];
 $Comentarios=$_Get['Comentarios'];
 
 
-$conexion=mysqli_connect("localhost","root","","ibq");
+
 
 $sqlInsertTipo="INSERT INTO persona(id_persona,nombre_persona,ape_pat,ape_mat,tel_persona,cargo_persona,coment_persona) VALUES($id_persona,$Nombre,$Apellido_Paterno,$Apellido_Materno,$Cargo,$Telefono,$Comentarios)";
 
