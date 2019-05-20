de907cdc688ca0c0fa908d6476e5c3223f2ce8d2
Gabriel Orive <35276201+Gaboshok@users.noreply.github.com>
2019-05-20 18:47:44 -0500

    Se ordenaron carpetas
    se ordenaron carpetas y se elimino las imagenes sobrantes

--- BD/borrarArticulo.php
+++
@@ -1,8 +0,0 @@
-<?php 
-require_once("Conexion.php");
-$id=$_POST['id']; 
-$model=new Conexion();
-$query="DELETE FROM articulo WHERE id_articulo=$id;";
-$model->set_query($query);
-header("Location:../bajas.html");
- ?>
